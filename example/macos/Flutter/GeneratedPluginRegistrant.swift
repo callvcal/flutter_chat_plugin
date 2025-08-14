694bf1fd144797f19b3c25953035a4572fa6805d
Callvcal <tech.callvcal@gmail.com>
2025-08-14 14:16:56 +0530

    first-commit

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import chat_plugin
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ChatPlugin.register(with: registry.registrar(forPlugin: "ChatPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
--- lib/chat_plugin.dart
+++ lib/chat_plugin.dart
@@ -1,8 +1,23 @@
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'chat/models/chat_models.dart';
 import 'chat_plugin_platform_interface.dart';
 
 class ChatPlugin {
   Future<String?> getPlatformVersion() {
     return ChatPluginPlatform.instance.getPlatformVersion();
+  }
+
+  Future<void> init() async {
+    // Initialize Hive with Flutter bindings
+    await Hive.initFlutter();
+
+    // Register all your Hive adapters
+    Hive.registerAdapter(ChatMessageAdapter());
+    Hive.registerAdapter(ChatTicketAdapter());
+
+    // Open the boxes AFTER registering adapters
+    await Hive.openBox<ChatMessage>('gs_rkhvk_chat_messages');
+    await Hive.openBox<ChatTicket>('gs_rkhvk_chat_tickets');
   }
 }
